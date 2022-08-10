0fc6350a54752a0daad58fee2e26fd127bd1da40
JaeYoonKimme <jeyun0219@naver.com>
2022-08-10 09:40:43 +0900

    Update RoutineList with sample data

--- src/pages/GoalList.js
+++ src/pages/GoalList.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
+
+const routine_list = require("../list_data.json");
 
 function GoalList() {
+  const listItems = routine_list.list.map((routine) =>
+    <li>
+      <Link to='/detail/'>{routine.title}</Link>
+      <br />
+      {routine.start_date} ~ {routine.start_date}
+      <br />
+      {routine.now_people_number}/{routine.max_people_number}
+    </li>
+  );
   return (
     <>
-      <h1>List</h1>
+      <h1>Routine List</h1>
+      <ul>
+      {listItems}
+      </ul>
+      <h2><Link to="/" >목표 만들기</Link></h2>
     </>
   )
 }
