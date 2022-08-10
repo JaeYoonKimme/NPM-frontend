338d06afd2f58aebeb6acf551d6b38160e7e6dcb
JaeYoonKimme <jeyun0219@naver.com>
2022-08-10 11:22:58 +0900

    Create Login Page

---
+++ src/pages/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+function Login() {
+  return (
+    <>
+      <h1>같은 목표를 가진 사람들과 함께 하니까 끝까지 해낼 수 있어요!</h1>
+      <h2>대한민국 1등 건강습관 앱, Erooming</h2>
+      <button>Sign In With Google</button>
+    </>
+  )
+}
+
+export default Login;
