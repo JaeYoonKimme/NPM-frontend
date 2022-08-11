2e797f56c6300aa9bee10a8b4111feab1578dd27
tjdgml97 <parksunghee9@naver.co>
2022-08-11 09:10:30 +0900

    update GoalDetailpage

--- src/pages/GoalDetail.js
+++ src/pages/GoalDetail.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 function GoalDetail() {
+
+  const location = useLocation();
+  console.log(location);
+  const id = location.state.id;
+  const title = location.state.title;
+  const start_date = location.state.start_date;
+  const end_date = location.state.end_date;
+  const des = location.state.description;
+
+
   return (
     <>
       <h1>Detail</h1>
+      <h2>{title}</h2>
+      <p>{des}</p>
+      <p>{start_date} ~ {end_date}</p>
     </>
   )
 }
