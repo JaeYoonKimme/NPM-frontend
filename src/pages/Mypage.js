631d257ba4f8ea2f43f8575927d25c3cdcfc13f8
JW-kim-k <tyty321321@gmail.com>
2022-08-18 17:21:50 +0900

    mypage sample

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+
 
 function Mypage({isLogin, info}) {
+  const [routines, setRoutines] = useState([]);
+
   const navigate = useNavigate();
   if (!isLogin) navigate('/');
+
+  const uid = 12345678
+
+  useEffect(() => {
+    axios
+      .get("http://" + process.env.REACT_APP_API_URL + `/api/user_routine_list/${uid}`
+      )
+      .then((res)=>{
+         setRoutines([...res.data]);
+      })
+      .catch((err) => console.log(err));
+    }, []);
 
   return (
     <>
@@ -14,6 +30,12 @@ function Mypage({isLogin, info}) {
       <h2>first name : {info.first_name}</h2>
       <h2>last name : {info.last_name}</h2>
       <h2>profile url : {info.profile_url}</h2>
+      <div style={{ fontSize: "3em" }}>Routine List</div>
+      <div>
+        {routines.map((routine, idx) => (
+          <RoutineContent key={idx} routine={routine} />
+        ))}
+      </div>
     </>
   )
 }
